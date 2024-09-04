7ff5686d09c213b0586f19a88ee409b7c7cea999
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-09-03 21:47:22 -0400

    Create a smoother transition

--- src/components/InfiniteScroll.js
+++ src/components/InfiniteScroll.js
@@ -13,26 +13,39 @@ const InfiniteScroll = () => {
     content.innerHTML = originalContent.innerHTML;
 
     const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = container;
+      const { scrollTop, clientHeight } = container;
+      const startElement = content.querySelector('#scroll-start');
+      const endElement = content.querySelector('#scroll-end');
       
-      if (scrollTop + clientHeight >= scrollHeight) {
-        // Reached bottom, jump to start position
-        const startElement = content.querySelector('#scroll-start');
-        if (startElement) {
-          container.scrollTop = startElement.offsetTop;
-        }
-      } else if (scrollTop <= 0) {
-        // Reached top, jump to end
-        const endElement = content.querySelector('#scroll-end');
-        if (endElement) {
-          container.scrollTop = endElement.offsetTop - clientHeight + endElement.offsetHeight;
-        } else {
-          container.scrollTop = scrollHeight - clientHeight;
+      console.log('Container clientHeight:', clientHeight);
+      console.log('Container scrollTop:', scrollTop);
+      
+      if (startElement && endElement) {
+        const containerRect = container.getBoundingClientRect();
+        const startRect = startElement.getBoundingClientRect();
+        const endRect = endElement.getBoundingClientRect();
+
+        // Calculate positions relative to the container
+        const startPosition = startRect.top - containerRect.top + scrollTop;
+        const endPosition = endRect.bottom - containerRect.top + scrollTop - clientHeight;
+
+        console.log('Start element position:', startPosition);
+        console.log('End element position:', endPosition);
+        console.log('Start element top relative to viewport:', startRect.top);
+        console.log('End element bottom relative to viewport:', endRect.bottom);
+
+        // Check if startRect is at or below the client height
+        if (startRect.top >= clientHeight) {
+          console.log('Start element is at or below viewport bottom, jumping to end');
+          container.scrollTop = endPosition;
+        } else if (endRect.bottom <= 0) {
+          console.log('End element is above viewport top, jumping to start');
+          container.scrollTop = startPosition;
         }
       }
     };
 
-    // Set initial scroll position to the scroll-start div
+    // Set initial scroll position to have scroll-start at the top
     const startElement = content.querySelector('#scroll-start');
     if (startElement) {
       container.scrollTop = startElement.offsetTop;
